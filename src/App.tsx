7ebfef0911be20c0ece3a21054837d0495fb88b7
Chris Guizzetti <chrisguizzetti@chriss-mbp.lan>
2024-05-20 15:24:13 -0700

    initial project setup with tailwind and prettier plugin setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full w-full bg-red-200 text-2xl text-red-950">
+      hi
+      <p className="flex h-full w-full bg-red-200 text-2xl text-red-950">
+        HEEEE
+      </p>
+    </div>
+  );
+}
+
+export default App;
